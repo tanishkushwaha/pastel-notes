50bc9d688cdf2dee750599fa5424801959812f37
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-07-16 02:21:05 +0530

    Add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const kollektif = localFont({
+  src: "../public/Kollektif.ttf",
+  display: "swap",
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
 
 export const metadata: Metadata = {
-  title: "Puddle Pad",
+  title: "Pastel Pad",
   description: "A Notes App",
 };
 
@@ -16,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={kollektif.className}>{children}</body>
     </html>
   );
 }
