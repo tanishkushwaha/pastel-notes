d99b5f6911b6d859251e9dab28090ee712f3758f
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-07-16 02:22:27 +0530

    Add SideDrawer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+'use client';
 
+import { useState } from "react";
+import { LuMenu } from "react-icons/lu";
+import SideDrawer from "@/components/SideDrawer";
 
 export default function Home() {
+  const [toggle, setToggle] = useState(false);
+
+  const toggler = () => {
+    setToggle(prevState => !prevState);
+  }
+
   return (
-    <div>Home</div>
+    <>
+      <div className="w-screen p-4 flex items-center">
+        <LuMenu className="size-8" onClick={toggler} />
+        <h1 className="m-auto text-2xl select-none">Pastel Notes</h1>
+      </div>
+      <SideDrawer toggle={toggle} toggler={toggler} />
+    </>
   )
 }
---
+++ components/SideDrawer.tsx
@@ -0,0 +1,38 @@
+import { FaRegNoteSticky } from "react-icons/fa6";
+import { LuArrowLeftToLine, LuArchive, LuTrash, LuLogOut } from "react-icons/lu";
+
+const SideDrawer = ({ toggle, toggler }: { toggle: boolean, toggler: () => void }) => {
+
+  return (
+    <div className={`bg-white h-screen absolute top-0 left-0 ${toggle ? 'w-1/5 p-4' : 'w-0 p-0'} transition-all overflow-hidden drop-shadow-xl flex flex-col`}>
+      <LuArrowLeftToLine className="size-8 mb-8" onClick={toggler} />
+      <Menu />
+    </div>
+  );
+}
+
+const Menu = () => {
+
+  return (
+    <ul className="h-auto flex flex-col flex-1">
+      <li className="p-4 hover:bg-gray-100 text-lg flex items-center">
+        <FaRegNoteSticky className="mr-2" />
+        Notes
+      </li>
+      <li className="p-4 hover:bg-gray-100 text-lg flex items-center">
+        <LuArchive className="mr-2" />
+        Archive
+      </li>
+      <li className="p-4 hover:bg-gray-100 text-lg flex items-center">
+        <LuTrash className="mr-2" />
+        Trash
+      </li>
+      <li className="p-4 hover:bg-gray-100 text-lg mt-auto flex items-center">
+        <LuLogOut className="mr-2" />
+        Logout
+      </li>
+    </ul>
+  );
+}
+
+export default SideDrawer;
