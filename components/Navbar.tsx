b04f222988bcf759bb085833dfea4e86d93ccfb3
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-07-20 19:36:27 +0530

    Change navbar title to 'Pastel Notes'

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,10 @@
 import { LuMenu } from "react-icons/lu";
 
-
 export default function Navbar({ sideDrawerToggler }: { sideDrawerToggler: () => void }) {
   return (
     <div className="flex items-center p-4">
       <LuMenu className="size-8" onClick={sideDrawerToggler} />
-      <h1 className="text-2xl mx-auto select-none">Something</h1>
+      <h1 className="text-2xl mx-auto select-none">Pastel Notes</h1>
     </div>
   )
 }
