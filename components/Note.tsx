5e2f5dc11f7699fbfc3f3214b7db359538a49de9
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-07-20 19:35:48 +0530

    Add text ellipsis in the note component

--- components/Note.tsx
+++ components/Note.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 export default function Note({ title, body }: { title: string, body: string }) {
 
-
   return (
-    <div className={`w-64 h-64 bg-pastelYellow rounded-xl p-4 flex flex-col`}>
+    <div className={`w-64 min-h-64 bg-pastelYellow rounded-xl p-4 flex flex-col`}>
       <h1 className="text-2xl mb-4">{title}</h1>
-      <div className="text-lg overflow-hidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate doloremque vel, maiores non obcaecati laudantium, amet atque in dolores excepturi est harum deserunt minima distinctio quia inventore et eveniet eligendi.</div>
+      <div className="text-lg line-clamp-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate doloremque vel, maiores non obcaecati laudantium, amet atque in dolores excepturi est harum deserunt minima distinctio quia inventore et eveniet eligendi.</div>
     </div>
   )
 }
