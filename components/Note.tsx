6a2bc7ef49bdda9c43cf7dcc09fd78914ebad9d9
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-07-21 19:34:08 +0530

    Add light box modal

--- components/Note.tsx
+++ components/Note.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-export default function Note({ title, body }: { title: string, body: string }) {
+function LightBox({ title, body, open, setOpen }: { title: string, body: string, open: boolean, setOpen: React.Dispatch<React.SetStateAction<boolean>> }) {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (open) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [open]);
+
+  const handleClickOutside = (e: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+      setOpen(false);
+    }
+  };
 
   return (
-    <div className={`w-64 min-h-64 bg-pastelYellow rounded-xl p-4 flex flex-col`}>
-      <h1 className="text-2xl mb-4">{title}</h1>
-      <div className="text-lg line-clamp-6">{body}</div>
+    <div className={`fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 ${open ? 'flex' : 'hidden'} justify-center items-center`}>
+      <div className="bg-pastelYellow p-4 rounded-xl min-h-96 w-2/4" ref={modalRef}>
+        <h1 className="text-2xl">{title}</h1>
+        <p className="text-lg">{body}</p>
+      </div>
     </div>
   )
 }
+
+export default function Note({ title, body }: { title: string, body: string }) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <LightBox title={title} body={body} open={open} setOpen={setOpen} />
+      <div className={`w-64 min-h-64 bg-pastelYellow rounded-xl p-4 flex flex-col`} onClick={() => setOpen(true)}>
+        <h1 className="text-2xl mb-4">{title}</h1>
+        <div className="text-lg line-clamp-6">{body}</div>
+      </div>
+    </>
+  )
+}
+
+
