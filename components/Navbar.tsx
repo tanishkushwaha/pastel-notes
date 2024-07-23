01eb9ca9aae43da4dd12535b8e9bb55b94636bb6
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-07-23 18:56:47 +0530

    Add logic for not showing navbar on certain routes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,15 +3,25 @@
 import useDisclosure from "@/hooks/useDisclosure";
 import Header from "./Header";
 import SideDrawer from "./SideDrawer";
+import { usePathname } from "next/navigation";
 
 
 export default function Navbar() {
   const sideDrawer = useDisclosure();
+  const pathname = usePathname();
+
+  const noNavbarRoutes = ["/signin", "/signup"];
+
+  const showNavbar = !noNavbarRoutes.includes(pathname);
 
   return (
     <>
-      <SideDrawer open={sideDrawer.open} onClose={sideDrawer.onClose} />
-      <Header sideDrawerOpener={sideDrawer.onOpen} />
+      {showNavbar && (
+        <>
+          <SideDrawer open={sideDrawer.open} onClose={sideDrawer.onClose} />
+          <Header sideDrawerOpener={sideDrawer.onOpen} />
+        </>
+      )}
     </>
   )
 }
